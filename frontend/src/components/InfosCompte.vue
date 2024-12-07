6f44cfed5100907d8daa5925a7b6c7a251b724e9
GalateeM <galatee.m@gmail.com>
2024-12-07 13:29:39 +0100

    pas besoin de cette ligne

--- frontend/src/components/InfosCompte.vue
+++ frontend/src/components/InfosCompte.vue
@@ -462,7 +462,6 @@ function createRecordBar(ctx) {
     data: {
       labels: categoriesRecord,
       datasets: [{
-        label: 'Record of correct answers',
         data: records, 
         backgroundColor: extendedPalette,
         borderWidth: 1,
